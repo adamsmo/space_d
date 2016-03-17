e0b6634a0732064bbdc63baddde55d025f7ca4ca
adamsmo <adam.smolarek@scalac.io>
2016-03-17 18:11:59 +0100

    init

---
+++ Assets/m_project/GravityAttractor.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class GravityAttractor : MonoBehaviour {
+
+	public float gravity = -9.8f;
+	public float gravityRange = 6;
+	public float atmosphereRadiouse = 100;
+
+	public void Attract(Rigidbody body) {
+		Vector3 gravityUp = (body.position - transform.position).normalized;
+		Vector3 localUp = body.transform.up;
+
+		// Apply downwards gravity to body
+		body.AddForce(gravityUp * gravity);
+
+
+		float distance = Vector3.Distance (transform.position, body.transform.position);
+
+		if(distance < atmosphereRadiouse){
+			atmBreak (body);
+		}
+
+
+		// Allign bodies up axis with the centre of planet
+		body.rotation = Quaternion.FromToRotation(localUp,gravityUp) * body.rotation;
+
+
+	}  
+
+
+	public void atmBreak(Rigidbody body){
+		float atmBreaking = 3;
+		var curSpeed = body.velocity.magnitude;
+		var newSpeed = curSpeed - atmBreaking * Time.deltaTime;
+		if (newSpeed < 0) {
+			newSpeed = 0;
+		}
+		body.velocity = body.velocity.normalized * newSpeed;
+	}
+}
---
+++ Assets/m_project/Utils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AssemblyCSharp
+{
+	public class Utils
+	{
+		public static T fold<T>(Func<T, T, T> f, T[] coll){
+			if (coll.Length == 1) {
+				return default(T);
+			} else if (coll.Length == 1) {
+				return coll [0];
+			} 
+			T result = coll[0];
+			for(int i = 1; i < coll.Length; i++){
+				result = f (result, coll [i]);
+			}
+			return result;
+		}
+	}
+}
+
