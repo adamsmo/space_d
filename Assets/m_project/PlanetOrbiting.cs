aa7fa8db5cc27b7193bc2caa2fc9232dd747df57
adamsmo <adam.smolarek@scalac.io>
2016-03-18 16:20:51 +0100

    prevent planets from 'running away'

---
+++ Assets/m_project/PlanetOrbiting.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class PlanetOrbiting : MonoBehaviour {
+
+	public float planetSpeed = 2;
+
+	GameObject sun;
+
+	void Awake () {
+		findSun();
+	}
+
+	void FixedUpdate () {
+		transform.RotateAround (sun.transform.position, sun.transform.up, planetSpeed);
+	}
+
+	void OnDrawGizmosSelected() {
+		findSun ();
+		Handles.color = Color.red;
+		float distance = Vector3.Distance (sun.transform.position, transform.position);
+		Handles.DrawWireDisc(sun.transform.position, sun.transform.up, distance);
+	}
+		
+	void findSun(){
+		sun = GameObject.FindGameObjectWithTag ("Sun");
+	}
+}
